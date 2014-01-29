415517fafd6272ff473e50eedc09617844c370ca
osiepna <jferidrico@gmail.com>
2014-01-29 13:38:13 +0300

    Add everything checking ..

--- checktask.php
+++ checktask.php
@@ -1,10 +1,9 @@
 <?php
 	$str = intval($_GET['str']);
-	$str = trim($str);
 	$con = mysqli_connect('localhost', 'root', 'Udonis 1', 'todoList');
         if (!$con) {
           die('Could not connect: ' . mysql_error());
         }
         echo "Hello";
-        mysqli_query($con,"UPDATE Tasks SET Status=1 WHERE TaskName='$str'");
+        mysqli_query($con,"UPDATE Tasks SET Status=1 WHERE TaskName=$str");
         mysqli_close($con);
